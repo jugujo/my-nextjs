2a341fa558fd6649ef7ed7e76c39a3b6dbbc3b42
jugujo <jugujoo@hotmail.com>
2023-11-05 22:13:02 +0900

    リンク追加

--- app/projects/page.js
+++ app/projects/page.js
@@ -2,6 +2,7 @@
 import MainContents from '@/components/home/mainContents';
 import { Client } from "@notionhq/client";
 import ProjectItem from "../../components/projects/item";
+import Link from 'next/link';
 
 export const databaseId = process.env.NOTION_DATABASE_ID;
 
@@ -10,9 +11,11 @@ export default async function Project() {
   const list = await getData()
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen px-3 mb-10">
+    <div>
       <title>プロジェクト</title>
       <div className="grid grid-cols-1 gap-8 p-12 m-4 md:grid-cols-2">
+      <Link href="https://www.notion.so/c50f7095a9f54d599b81b9c81758b61b?v=f5f22d4f0b174cfca90ea70ddfc45023" className=" justify-left hover:text-gray-900" target='_blank'>※ノーション情報と連携</Link>
+      <p></p>
         {list.map((item, index) => (
           <ProjectItem key={item.id} data={item}/>
         ))}
